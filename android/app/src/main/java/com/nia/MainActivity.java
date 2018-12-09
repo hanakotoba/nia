13273241b9a6bceb3fe42d4fd37bb8ac5275c23f
Bogdan Volyk <nani@omaewamoushindei.ru>
2018-12-09 04:54:34 +0200

    🔧 Add react-native-navigation

--- android/app/src/main/java/com/nia/MainActivity.java
+++ android/app/src/main/java/com/nia/MainActivity.java
@@ -1,15 +1,19 @@
 package com.nia;
 
-import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+// public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "nia";
-    }
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "nia";
+//     }
+// }
+
+public class MainActivity extends NavigationActivity {
 }
